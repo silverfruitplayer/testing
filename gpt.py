fec5771b9ca6ca9299faac7c8f27e9adc95c1106
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2024-01-25 14:37:37 +0530

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -2,8 +2,9 @@
 #from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from random import choice
 from pyrogram import Client, filters, idle
-#from aiohttp import ClientSession
+from aiohttp import ClientSession
 #from json import loads
+from httpx import AsyncClient, Timeout
 #from pyrogram.types import Message as message
 import asyncio
 #import openai
@@ -11,13 +12,29 @@ import requests
 import logging
 import os
 
-GOOGLEAI_KEY = "AIzaSyC2cKZRxUsoCfYaveyab08QEp7jxsRWrJk"
+GOOGLEAI_KEY = ""
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO)
 
-app = Client("gptbot", bot_token="6560962385:AAHwbTlxi7ntT6y6c8rfgu4_zgXw9y2vq2Q", api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+app = Client("gptbot", bot_token="", api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
+
+# Aiohttp Async Client
+session = ClientSession()
+
+# HTTPx Async Client
+fetch = AsyncClient(
+    http2=True,
+    verify=False,
+    headers={
+        "Accept-Language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
+    },
+    timeout=Timeout(20),
+)
+
 
 @app.on_message(filters.command("start"))
 async def start(_, message):
